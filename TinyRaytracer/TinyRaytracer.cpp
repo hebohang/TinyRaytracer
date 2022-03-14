e913e194f1301671adf981b57cd2bb05059af7b1
hebohang <hebohang1113@gmail.com>
2022-03-14 12:00:01 +0800

    Hello, stb!

---
+++ TinyRaytracer/TinyRaytracer.cpp
@@ -0,0 +1,13 @@
+﻿#include "stb_image.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+    const std::string ProjPath(PROJECT_PATH);
+    std::string PhotoPath = ProjPath + std::string("/resource/laotouhuan.jpg");
+    int weight, height, n;
+    unsigned char* idata = stbi_load(PhotoPath.c_str(), &weight, &height, &n, 0);
+    std::cout << "The photo weight: " << weight << std::endl;
+    std::cout << "The photo height: " << height << std::endl;
+}
